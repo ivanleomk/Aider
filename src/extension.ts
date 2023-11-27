f230ed1492cd1e3473f26cafb2cfa414478a857f
Ivan <ivanleomk@gmail.com>
2023-11-27 22:54:27 +0800

    tmp

---
+++ src/extension.ts
@@ -0,0 +1,232 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { resolve } from "path";
+import * as vscode from "vscode";
+import * as path from "path";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("aider.debugChat", async () => {
+      ReactPanel.createOrShow(
+        context.extensionPath.replace("build", "webviews/build")
+      );
+    })
+  );
+
+  const prompt_suggestion = vscode.commands.registerCommand(
+    "aider.generateAutocomplete",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (!editor) {
+        return;
+      }
+
+      const selection = editor.selection;
+      const selectedText = editor.document.getText(selection);
+      const action = await vscode.window.showQuickPick(
+        ["Generate Autocomplete", "Debug"],
+        {
+          placeHolder: "Choose an action",
+        }
+      );
+      if (action === "Generate Autocomplete") {
+        const res = await fetch("http://0.0.0.0:8000/code/completion", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            code: selectedText,
+          }),
+        });
+
+        const start = new Date().getTime();
+        const response = await res.json();
+        const end = new Date().getTime();
+        const time = end - start;
+
+        if (
+          response &&
+          typeof response === "object" &&
+          "completion" in response
+        ) {
+          // Write into the user field some code
+          editor.edit((editBuilder) => {
+            editBuilder.insert(selection.end, response["completion"] as string);
+          });
+          vscode.window.showInformationMessage(
+            `Generated completion in ${time}s`
+          );
+
+          setTimeout(() => {
+            vscode.window.showInformationMessage("");
+          }, 1000);
+        }
+      }
+    }
+  );
+
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+
+  context.subscriptions.push(prompt_suggestion);
+}
+
+/**
+ * Manages react webview panels
+ */
+export class ReactPanel {
+  /**
+   * Track the currently panel. Only allow a single panel to exist at a time.
+   */
+  public static currentPanel: ReactPanel | undefined;
+
+  private static readonly viewType = "react";
+
+  private readonly _panel: vscode.WebviewPanel;
+  private readonly _extensionPath: string;
+  private _disposables: vscode.Disposable[] = [];
+
+  public static createOrShow(extensionPath: string) {
+    const column = vscode.window.activeTextEditor
+      ? vscode.window.activeTextEditor.viewColumn
+      : undefined;
+
+    // If we already have a panel, show it.
+    // Otherwise, create a new panel.
+    if (ReactPanel.currentPanel) {
+      ReactPanel.currentPanel._panel.reveal(column);
+    } else {
+      ReactPanel.currentPanel = new ReactPanel(
+        extensionPath,
+        column || vscode.ViewColumn.One
+      );
+    }
+  }
+
+  private constructor(extensionPath: string, column: vscode.ViewColumn) {
+    console.log(extensionPath);
+    this._extensionPath = extensionPath;
+
+    // Create and show a new webview panel
+    this._panel = vscode.window.createWebviewPanel(
+      ReactPanel.viewType,
+      "React",
+      column,
+      {
+        // Enable javascript in the webview
+        enableScripts: true,
+
+        // And restric the webview to only loading content from our extension's `media` directory.
+        localResourceRoots: [
+          vscode.Uri.file(path.join(this._extensionPath, "build")),
+        ],
+      }
+    );
+
+    // Set the webview's initial html content
+    this._panel.webview.html = this._getHtmlForWebview();
+
+    // Listen for when the panel is disposed
+    // This happens when the user closes the panel or when the panel is closed programatically
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+    // Handle messages from the webview
+    this._panel.webview.onDidReceiveMessage(
+      (message) => {
+        switch (message.command) {
+          case "alert":
+            vscode.window.showErrorMessage(message.text);
+            return;
+        }
+      },
+      null,
+      this._disposables
+    );
+  }
+
+  public doRefactor() {
+    // Send a message to the webview webview.
+    // You can send any JSON serializable data.
+    this._panel.webview.postMessage({ command: "refactor" });
+  }
+
+  public dispose() {
+    ReactPanel.currentPanel = undefined;
+
+    // Clean up our resources
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const x = this._disposables.pop();
+      if (x) {
+        x.dispose();
+      }
+    }
+  }
+
+  private _getHtmlForWebview() {
+    const manifest = require(path.join(
+      this._extensionPath,
+      "build",
+      "asset-manifest.json"
+    ));
+    const mainScript = manifest["files"]["main.js"];
+    const mainStyle = manifest["files"]["main.css"];
+
+    const scriptPathOnDisk = vscode.Uri.file(
+      path.join(this._extensionPath, "build", mainScript)
+    );
+    const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
+    const stylePathOnDisk = vscode.Uri.file(
+      path.join(this._extensionPath, "build", mainStyle)
+    );
+    const styleUri = stylePathOnDisk.with({ scheme: "vscode-resource" });
+
+    // Use a nonce to whitelist which scripts can be run
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+				<meta name="theme-color" content="#000000">
+				<title>React App</title>
+				<link rel="stylesheet" type="text/css" href="${styleUri}">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
+				<base href="${vscode.Uri.file(path.join(this._extensionPath, "build")).with({
+          scheme: "vscode-resource",
+        })}/">
+			</head>
+
+			<body>
+				<noscript>You need to enable JavaScript to run this app.</noscript>
+				<div id="root"></div>
+				
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+  }
+}
+
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
