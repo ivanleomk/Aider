299b71a61cb0cc30f493824504a6499c1aca17bf
Ivan <ivanleomk@gmail.com>
2023-11-28 22:12:37 +0800

    Fixed up shadcn-ui

--- webviews/src/App.tsx
+++ webviews/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Button } from "./components/button";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Button>Click Me!</Button>
     </div>
   );
 }
